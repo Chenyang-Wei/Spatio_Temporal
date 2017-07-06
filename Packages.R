0d3f3be792b746c36f042f417f76d4e91212055a
Chenyang-Wei <cwei5@buffalo.edu>
2017-07-06 09:22:02 -0400

    Install the related packages.

---
+++ Packages.R
@@ -0,0 +1,5 @@
+install.packages(pkgs = c("spBayes","MBA","geoR","raster","fields",
+                          "rgl","sp","maptools","rgdal","classInt",
+                          "gstat","lattice","xtable","cluster","e1071", 
+                          "spam", "yaImpute","RgoogleMaps"), 
+                 repos = "http://cran.r-project.org")
